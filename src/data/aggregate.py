f099ae27dcaf4564a5fabdfafc725b21e35de7c7
leolmeta <lolmetab@gmail.com>
2026-01-02 20:43:21 -0500

    fixed image rendering issues

--- src/data/aggregate.py
+++ src/data/aggregate.py
@@ -21,7 +21,7 @@ def main(eng_dict):
 
     cols = ['ticker', 'Date'] + [c for c in mega_df.columns if c not in ['ticker', 'Date']]
 
-    return mega_df[cols]
+    return mega_df[cols].sort_values('Date')
 
 if __name__ == '__main__':
     main()
